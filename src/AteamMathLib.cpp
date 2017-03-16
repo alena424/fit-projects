370ec39200e2120ff781dd01374d5b0e92c98fad
Daniel Uhricek <daniel.uhricek@gypri.cz>
2017-03-16 18:58:45 +0100

    Added AteamMathLib.cpp

---
+++ src/AteamMathLib.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file AteamMathLib.cpp
+ *
+ * @brief Definitions of functions in math library.
+ *
+ * File containing definitions of functions, their 
+ * implementation etc.
+ *
+ * @author Daniel Uhricek (xuhric00)
+ * @author Peter Uhrin (xuhrin02)
+ * @author Alena Tesarova (xtesar36)
+ * @author Jan Sorm (xsorjm00)
+ */
+
+
+#include "AteamMathLib.h"
+#include <stdexcept>
+
+namespace MathLib
+{
+
+  unsigned long long int AteamMathLib::factorial (unsigned short int x) {
+    // IMPLEMENTATION
+  }
+
+  double AteamMathLib::power (double x, unsigned int n) {
+    // IMPLEMENTATION 
+  }
+
+  double AteamMathLib::nroot (double x, unsigned int n) {
+    // IMLEMENTATION 
+  }
+
+}
+
