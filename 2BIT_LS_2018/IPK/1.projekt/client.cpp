17440aabbbbc72b787b913c302e78ba812ca2255
Tesarova Alena <xtesar36@merlin.fit.vutbr.cz>
2018-03-10 21:24:33 +0100

    send solved

--- 2BIT_LS_2018/IPK/1.projekt/client.cpp
+++ 2BIT_LS_2018/IPK/1.projekt/client.cpp
@@ -7,6 +7,10 @@
 
 #include <iostream>
 #include <string.h>
+#include <iostream>
+#include <sstream>
+#include <map>
+
 
 #include <time.h>
 
@@ -20,34 +24,25 @@
 
 using namespace std;
 
-#define BUFSIZE 1024 //nejvetsi soubor
+#define BUFSIZE 2024  //nejvetsi soubor
 #define MAX_LOGIN 10
 #define TIME_STRING 100 // makro pro ukladani casu
 #define KEY 3 //klic k kodovani
 
-class Message {
+class Message
+{
 private:
     string option;
     string login;
     string message;
-    /**
-    * cryptLogin - zakodovani loginu to naseho kodovani US3LET
-    */
-    void cryptLogin( ){
-        string log_crypt = this->login;
-        for ( auto it = log_crypt.begin(); it < log_crypt.end(); it++ ) {
-            // prochazim string
-            *it = *it + KEY;
-        }
-        cout << "LOGIN: " << log_crypt;
-        this->login = log_crypt;
-    }
+
 public:
 
     /**
     * Konstruktor
     */
-    Message(string option, string login){
+    Message(string option, string login)
+    {
         this->login = login;
         this->option = option;
         this->message = "";
@@ -57,7 +52,8 @@ public:
     /**
     * createMessage - zapozdreni informaci do zpravy
     */
-    void createMessage(){
+    void createMessage()
+    {
         this->message = "IPKAPP/1.1.\r\n";
 
         // jake je dnesni datum
@@ -66,20 +62,101 @@ public:
         char string_time[TIME_STRING];
         strftime(string_time, 128, "Date: %a, %d %b %Y %H:%M:%S %Z\r\n", &tm);
         this->message += string_time;
-        this->message += "Encrypted: US3LET\r\n";
         this->message += "Content-Length: " + to_string ( login.length() ) + "\r\n";
         this->message += "ExpectedOutput: " +  option + "\r\n";
-        this->message += "Data: " + this->login + "\r\n";
+        this->message += "Data: " + this->login + "\n";
 
     }
-    string getMessage(){
+    string getMessage()
+    {
         return this->message;
     }
 
 };
+class Response
+{
+public:
+
+    int total_messages = 0; // aspon jedna pokud se pouziva tato trida
+    int number_message = 0;
+    string data = "";
+    string error_message = "";
+
+    Response( )
+    {
+        this->data = "";
+    }
+    string getData(string message, string toExtract)
+    {
+        // jedem po radku dokud nenajdeme contentlength
+
+        stringstream os;
+        os.str(message);
+        string line;
+        while ( getline( os, line))
+        {
+            int index = line.find( toExtract ); // hledame informaci, kteru chceme zobracit
+            if ( index != string::npos )
+            {
+                // nasli jsme
+                line.erase( 0, index +toExtract.length() );
+                line.erase(line.find_last_not_of(" \n\r\t") +1); // zbavime se koncovych mezer
+                //cout << "LINE erased:"<< line << endl;
+                //cout << line;
+                return (line);
+            }
+        }
+        return "";
+    }
+    void parse( stringstream& message )
+    {
+        // potrebuju ji rozparsovat na map
+        string line;
+        map <string, string> received_loc;
+
+        while ( getline( message, line) )
+        {
+            int index = line.find( ": " ); // hledame informaci, kteru chceme zobracit
+            if ( index != string::npos )
+            {
+                // nasli jsme
+                received_loc[ line.substr(0, index) ] = line.substr(index+2 ); // parsuju zpravu
+            }
+            else
+            {
+                //pokud cteme z dat, bereme cely radek
+                received_loc[ "Data" ] += line;
+            }
+        }
+        //cout << "PARSE: " << received_loc[ "Data" ] << endl;
+    }
+
+    //pridame si dalsi data to koncove zpravy
+    string returnData( )
+    {
+        string newdata = this->data;
+        string data_status = getData( newdata, "RESP: " );
+        int index = newdata.find( "Data: ");
+
+        if ( data_status.compare("OK") != 0 )
+        {
+            // neni to ok
+            this->error_message = data_status;
+            return "";
+        }
+        int data_length = stoi(getData( newdata, "Content-Length: " ) );
+        if ( index != string::npos )
+        {
+            return newdata.substr(index+6, data_length);
+        }
+        return "";
+    }
+};
 
 
-int main (int argc, const char * argv[]) {
+
+int main (int argc, const char * argv[])
+{
 
     bool login; // zda byl zadan login nebo ne
     string regex_login ; // zadany login v argumentecg
@@ -90,20 +167,28 @@ int main (int argc, const char * argv[]) {
     const char *server_hostname;
 
     // priklad ./ipk-client -h host -p port [-n|-f|-l] login
-    if ( argc <= 5 || argc >= 8 || ( strcmp( argv[1], "-h" ) != 0 ) || ( strcmp( argv[3], "-p" ) != 0 ) ){
+    if ( argc <= 5 || argc >= 8 || ( strcmp( argv[1], "-h" ) != 0 ) || ( strcmp( argv[3], "-p" ) != 0 ) )
+    {
         // argumentu je minimalne 6 a maximalne 7
         cerr << "ERROR: Bad format of arguments\n, example: " << argv[0] << " -h host -p port [-n|-f|-l] login\n" << endl ;
         exit(EXIT_FAILURE);
     }
     // kontrola [-n|-f|-l]
 
-    if ( strcmp( argv[5], "-n" ) == 0 )  {
+    if ( strcmp( argv[5], "-n" ) == 0 )
+    {
         option = "N";
-    } else if ( strcmp( argv[5], "-f" ) == 0 )  {
-         option = "F";
-    } else if ( strcmp( argv[5], "-l" ) == 0 )  {
+    }
+    else if ( strcmp( argv[5], "-f" ) == 0 )
+    {
+        option = "F";
+    }
+    else if ( strcmp( argv[5], "-l" ) == 0 )
+    {
         option = "L";
-    } else {
+    }
+    else
+    {
         cerr << "ERROR: Bad format of arguments\n, example: " << argv[0] << " -h host -p port [-n|-f|-l] login\n" << endl ;
         exit(EXIT_FAILURE);
     }
@@ -111,23 +196,27 @@ int main (int argc, const char * argv[]) {
     server_hostname = argv[2];
     port_number = atoi(argv[4]);
 
-    if ( argc == 7 && strlen(argv[6]) < 10) {
+    if ( argc == 7 && strlen(argv[6]) < 10)
+    {
         regex_login = argv[6];
         login = "1";
-    } else {
-         regex_login = "";
-         login = "0";
+    }
+    else
+    {
+        regex_login = "";
+        login = "0";
     }
 
     // pripojeni
-	int client_socket;
+    int client_socket;
     socklen_t serverlen;
     struct hostent *server;
     struct sockaddr_in server_address;
 
 
     // ziskani adresy serveru pomoci DNS
-    if ((server = gethostbyname(server_hostname)) == NULL) {
+    if ((server = gethostbyname(server_hostname)) == NULL)
+    {
         cerr <<"ERROR: no host named " << server_hostname << "\n" << endl;
         exit(EXIT_FAILURE);
     }
@@ -140,49 +229,86 @@ int main (int argc, const char * argv[]) {
     server_address.sin_port = htons(port_number); // prevedeni do spravneho tvaru
 
     // tiskne informace o vzdalenem soketu pak smazat
-    printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
+    //printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
 
     // Vytvoreni soketu, SOCK_STREAM je pro TCP
-	if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
-	{
-		perror("ERROR: socket failed");
-		exit(EXIT_FAILURE);
-	}
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
+    {
+        perror("ERROR: socket failed");
+        exit(EXIT_FAILURE);
+    }
 
     // posilani zpravy na server, specifikovat, co po nem chceme
     char buf[BUFSIZE] = "";
     bzero(buf, BUFSIZE);
 
-    //strcat(buf, option);
-    //strcat(buf, login);
-    //strcat(buf, regex_login);
-    //buf[strlen(buf)] = '\0';
     Message mess ( option, regex_login );
     mess.createMessage();
     string output = mess.getMessage();
-    cout << output;
+    //cout << output;
     strcpy(buf, output.c_str() );
 
-    printf("delka je: %d", strlen(buf));
+    //printf("delka je: %d", strlen(buf));
 
     if (connect(client_socket, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0)
     {
-		perror("ERROR: connect");
-		exit(EXIT_FAILURE);
+        cerr << "ERROR: connect" << endl;
+        exit(EXIT_FAILURE);
     }
 
-	// odeslani zpravy na server
-	int send_resp = send(client_socket, buf, strlen(buf), 0);
+    // odeslani zpravy na server
+    int send_resp = send(client_socket, buf, strlen(buf), 0);
+    //cout << send_resp << endl;
     if (send_resp < 0)
-      perror("ERROR in sendto");
+    {
+        cerr << "ERROR in sendto";
+        close(client_socket);
+        exit(EXIT_FAILURE);
+
+    }
 
     // prijeti odpovedi
-    char message[BUFSIZE] = "";
-    int receive_mess = recv(client_socket, message, BUFSIZE, 0);
-    if (receive_mess < 0)
-      perror("ERROR in recvfrom");
+    int receive_mess = 0;
+    Response resp;
+    bool skip = false; // flag na podminku odpovedi
+    while(true)
+    {
+        //cout << i << endl;
+        char message[BUFSIZE] = "";
+        memset(message, 0, BUFSIZE);
+        receive_mess = recv(client_socket, message, BUFSIZE, 0);
 
-    printf("Echo from server: %s\n", message);
+        if (receive_mess < 0)
+        {
+            cerr <<"ERROR in recv" << endl;
+            exit(EXIT_FAILURE);
+        }
+        else if ( receive_mess == 0 )
+        {
+            break;
+        }
+        //printf("\n\n___________________________\n");
+        //printf("REC: %d \n", receive_mess);
+        //cout << "MESS CHAR:" << message << endl;
+        resp.data += message;
+
+        // nejaka chyba napr. neexistujici login
+        /*if ( resp.addData( message ) == false && (! skip ) )
+        {
+            cerr << "Unexisting login" << endl;
+            return EXIT_FAILURE;
+        }*/
+        skip = true; // login je spravny
+        //printf("___________________________\n");
+
+    }
+    string return_data = resp.returnData();
+    if ( resp.error_message.length() > 0 ){
+            cerr << resp.error_message << endl;
+            return EXIT_FAILURE;
+    } else {
+        cout << return_data;
+    }
 
     close(client_socket);
     return 0;
--- 2BIT_LS_2018/IPK/1.projekt/server.cpp
+++ 2BIT_LS_2018/IPK/1.projekt/server.cpp
@@ -11,7 +11,6 @@
 #include <sstream>
 #include <vector>
 
-#include <time.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -21,11 +20,12 @@
 #include <stdbool.h>
 
 #include <fcntl.h>
+#include <signal.h>
 
-#define BUFSIZE 2024
+#define BUFSIZE 1025
 #define LINEMAX 1024
 #define FILESIZE 2024
-#define TIME_STRING 100 // makro pro ukladani casu
+#define PARSE_LENGTH 947 //maximlani delka dat, protoze hlavicka nejdelsi muze mit delku 75 - 2 pro jistotu
 
 using namespace std;
 
@@ -38,43 +38,9 @@ private:
     stringstream message ;
     FILE *file;
     string output_data = "";
-    string output = "";
-    int key = 0;
 
     /**
-    * decryptLogin - dekodovani loginu to naseho kodovani
-    */
-    void decryptLogin()
-    {
-        //cout <<"dekodovano, klic je" << this->key << endl;
-        string log_crypt = this->login;
-        for ( auto it = log_crypt.begin(); it < log_crypt.end(); it++ )
-        {
-            // prochazim string
-
-            *it = *it - this->key;
-        }
-        //cout << "LOGIN DEC: " << log_crypt << endl;
-        this->login = log_crypt;
-    }
-
-    /**
-    * cryptLogin - zakodovani loginu to naseho kodovani US3LET
-    */
-    string cryptOutputData()
-    {
-        string log_crypt = this->output_data;
-        for ( auto it = log_crypt.begin(); it < log_crypt.end(); it++ )
-        {
-            // prochazim string
-            *it = *it + this->key;
-        }
-        //cout << "DATA: " << log_crypt;
-        this->output_data = log_crypt;
-    }
-
-    /**
-     * function getRecord
+     * method getRecord
      * @param line - one line of /etc/passwd that we want
      * @param col - number of coulomb to specified output
      * @return final output string
@@ -95,9 +61,9 @@ private:
     }
 
     /**
-     * function search_login - return one information about login found in coulomb in file f
-     * @param col - number of coulomb to specified output
-     * @param strick - if it is set, we search all login, if not just prefix
+     * method search_login - return one information about login found in coulomb in file f
+     * @param col - number of coulomb to specified output in /etc/passwd
+     * @param strick - if it is set, we search login till ':' in etc/passwd, if not just prefix
     */
     void search_login( int col, bool strick )
     {
@@ -107,20 +73,15 @@ private:
         {
             int i = 0;
             int hit = 0;
-            //printf("%s", line);
-            //cout << line;
-            //cout << this->output_data  << endl;
             while ( this->login[i] != '\0' || line[i] != ':' )
             {
-                // musime osetrit pripad, kdy nejsou striktni podminky a nezadame zadny prefix, delka je 1 - pouze '\0'
-                if ( strick == false && login.length() == 1)
+                // musime osetrit pripad, kdy nejsou striktni podminky a nezadame zadny prefix, delka je 0
+                if ( strick == false && login.length() == 0)
                 {
-                    //cout << "nenene" << endl;
                     this->output_data += line ;
                     break;
                 }
 
-                //cout << this->login[i] << endl;
                 if ( this->login[i] == line[i] )
                 {
 
@@ -129,26 +90,21 @@ private:
                     if ( strick == true)
                     {
                         // pokud budeme chtit striktne cely login, musime porovnavat s delkou retezce
-                        //cout << "HIT:" << hit << endl;
-                        if ( hit == this->login.length() -1 && line[hit] == ':')
+                        if ( hit == this->login.length()  && line[hit] == ':')
                         {
                             // bereme tento radek
-                            //cout << "HIT:" << hit << endl;
-                            //cout << this->login << endl;
                             this->output_data = getRecord( line, col );
+                            this->error = false;
                             return;
                         }
                     }
                     else
                     {
                         // staci at aspon 1 znak sedi
-                        // length bere i \0, konec retezce
-
-                        if ( hit == login.length() -1)
+                        if ( hit == login.length() )
                         {
                             // bereme tento radek
                             this->output_data += line ;
-
                         }
                     }
                 }
@@ -157,11 +113,14 @@ private:
                     break; // nerovna, muzu preskocit
                 }
                 i++;
-
             }
-        }
 
+        }
     }
+     /**
+     * method extractFromMess - extract one information specified by extract
+     * @param toExtract - string to get information from, for example Data: - gets all data in out message
+    */
     string extractFromMess( string toExtract )
     {
         string line;
@@ -172,106 +131,105 @@ private:
             {
                 // nasli jsme
                 line.erase( 0, toExtract.length() +2 ); // +1 jedna dvojtecka, +1 mezera
-                //cout << "LINE erased:"<< line << endl;
                 return line;
             }
         }
         return "";
     }
-    void createFinalOutput()
+    /**
+     * method addHeader - adds header to data (length, status, data)
+     * @param data - to this data we are adding header
+    */
+    string addHeader(string data)
     {
-        cout << "adding headers.." << endl;
-        this->output = "IPKAPP/1.1. RESPONSE OK \r\n";
-        // jake je dnesni datum
-        time_t time_now = time(0);
-        struct tm tm = *gmtime(&time_now);
-        char string_time[TIME_STRING];
-        strftime(string_time, 128, "Date: %a, %d %b %Y %H:%M:%S %Z\r\n", &tm);
-        this->output += string_time;
-        if ( this->key == 3)
+        string status = "OK";
+        cout << "stat: " << this->error << endl;
+        // pokud se jedna o list, nevypisujeme nic o neexistujicim loginu
+        if ( this->error )
         {
-            this->output += "Encrypted: US3LET\r\n";
+            //neexistuje loogin a neni varianta L
+            status = this->err_message;
         }
-        this->output += "Content-Length: " + to_string ( this->output_data.length() ) + "\r\n";
-        this->output += "Output: " +  this->option + "\r\n";
-
-        //this->cryptOutputData();
-        this->output += "Data: " + this->output_data + "\r\n";
-        //cout << this->output << endl;
-        // zbavime se vsech mezer nakonci
-        this->output.erase(this->output.find_last_not_of(" \n\r\t") +1);
+        string header_plus_data =  "IPKAPP/1.1. RESP: " + status + "\n";
+        //header_plus_data += "MESSAGE: " + to_string( number_message ) + "/" + to_string( this->number_of_messages ) + "\n";
+        header_plus_data += "Content-Length: " + to_string ( data.length() ) + "\n";
+        header_plus_data += "Data: " +  data ;
+        return header_plus_data;
     }
 
 public:
+    bool error = true;
+    string err_message = "Unexisting login"; // defaultni chybova hlaska
 
     /**
     * Konstruktor
+    * saves string to stringstream this->message nad opens file
     */
     MessageParse( stringstream& message )
     {
         this->message.str( message.str());
         this->file = fopen( "/etc/passwd", "r" );
-        this->login += "xtesar";
-        this->option += "N";
     }
 
     /**
-     * function proceedOutput
-     * @param option - [N|F|L]; N: prints names, F: prints information about home directory, L: print list of users specified by login
-     * @param login - patter to search
-     * @return final output string
+     * method proceedOutput - Saves correct data to this->output_data according to option
     */
     void proceedOutput( )
     {
-        string key = extractFromMess( "Encrypted" );
         this->option = extractFromMess( "ExpectedOutput" );
         this->login = extractFromMess( "Data" );
-
         // odmazeme mezery
         this->option.erase(this->option.find_last_not_of(" \n\r\t") +1);
-        //this->login.erase(this->login.find_last_not_of(" \n\r\t") +1);
-
-        //cout << "klic je :" << key << endl;
-        // zjistujeme kodovani
-        if ( ( key.find( "US3LET") ) != string::npos )
-        {
-            this->key = 3; // pro toto kodovani je klic 3
-        }
-        //this->decryptLogin();
+        this->login.erase(this->login.find_last_not_of(" \n\r\t") +1);
 
         int ret = 0;
-        if ( option.compare( "N" ) == 0)
+        this->error = true; // defaultne chyba
+        cout << "option: " << this->option << endl;
+        if ( this->option.compare( "N" ) == 0)
         {
+            // 4. sloupec
             search_login(4, true);
-
+            cout << "stat1: " << this->error << endl;
         }
-        if ( option.compare( "F" ) == 0)
+        if ( this->option.compare( "F" ) == 0)
         {
+            // 5. sloupec
             search_login(5,  true );
         }
-        else
+        else if ( this->option.compare( "L" ) == 0)
         {
+            // chceme vse => 999
             search_login( 999, false);
+            this->error = false; // nikdy neni chyba pokud se jedna o L
 
         }
-        this->createFinalOutput();
     }
 
-    string getOutputData()
+    /**
+     * method sendData - sending all data (means output_data + header)
+    */
+    bool sendData(int csocket)
     {
-        return this->output_data;
-    }
-    string getOutput()
-    {
-        return this->output;
-    }
-    string getLogin()
-    {
-        return this->login;
-    }
-    string getOption()
-    {
-        return this->option;
+        string all_data = this->addHeader(this->output_data);
+        int all_data_length = all_data.length();
+        int once = 1;
+        while (once)
+        {
+            cout << all_data << endl;
+            int send_resp = send(csocket, all_data.c_str(), all_data_length, 0);
+            if (send_resp < 0)
+            {
+                cerr << "ERROR in sendto";
+                return false;
+
+            }
+            else if ( send_resp == 0 )
+            {
+                return true;
+            }
+            all_data.erase(0, send_resp);
+            once = 0;
+        }
     }
 
     ~MessageParse()
@@ -280,16 +238,25 @@ public:
 
 };
 
+int csocket;
+void quit(int signal)
+{
+    close(csocket);
+    exit(signal);
+}
+
 int main (int argc, const char * argv[])
 {
+    signal(SIGINT, quit);
     struct sockaddr_in server_socket;
     struct sockaddr_in client_socket;
     char str[INET6_ADDRSTRLEN];
     int port_number;
 
+    /* argumenty kontrola */
     if (argc != 3 || ( strcmp( argv[1], "-p" ) != 0 ) )
     {
-        fprintf(stderr,"bad number of arguments: %s -p <port>\n", argv[0]);
+        cerr <<"bad number of arguments: "<< argv[0] << "-p <port>" << endl;
         exit(EXIT_FAILURE);
     }
     port_number = atoi(argv[2]);
@@ -298,96 +265,94 @@ int main (int argc, const char * argv[])
     int welcome_socket;
     if ((welcome_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("ERROR: socket");
+        cerr << "ERROR: socket" << endl;
         exit(EXIT_FAILURE);
     }
 
-    printf("Iniclaizing\n");
     memset(&server_socket,0,sizeof(server_socket));
     server_socket.sin_family = AF_INET;
     server_socket.sin_addr.s_addr = INADDR_ANY;
     server_socket.sin_port = htons(port_number);
 
-    printf("Binding\n");
     if (( bind(welcome_socket, (struct sockaddr*)&server_socket, sizeof(server_socket))) < 0)
     {
-        perror("ERROR: Unable to bind");
+        cerr << "ERROR: Unable to bind" << endl;
         exit(EXIT_FAILURE);
     }
-    printf("Listening\n");
+
     if ((listen(welcome_socket, 1)) < 0)
     {
-        perror("ERROR: Unable to listen");
+        cerr << "ERROR: Unable to listen" << endl;
         exit(EXIT_FAILURE);
     }
+
     while( true )
     {
+        int error_resv = 0;
         // delka soketu klienta
         socklen_t client_socket_len=sizeof(client_socket);
-        int csocket = accept(welcome_socket, (struct sockaddr*)&client_socket, &client_socket_len);
+        csocket = accept(welcome_socket, (struct sockaddr*)&client_socket, &client_socket_len);
 
         // nastaveni neblokujiciho soketu
         int flags = fcntl(csocket, F_GETFL, 0);
         int rc = fcntl(csocket, F_SETFL, flags | O_NONBLOCK);
         if (rc < 0)
         {
-            perror("fcntl() failed");
+            cerr << "fcntl() failed" << endl;
             exit(EXIT_FAILURE);
         }
 
         if (csocket > 0)
         {
-            /*if(inet_ntop(AF_INET, &client_socket.sin_addr, str, sizeof(str)))
-            {
-                printf("INFO: New connection:\n");
-                printf("INFO: Client address is %s\n", str);
-                printf("INFO: Client port is %d\n", ntohs(client_socket.sin_port));
-            }*/
-            // buffer pro prijem
             char buff[BUFSIZE] = "";
+            memset(buff, 0, BUFSIZE);
 
-            int res = recv(csocket, buff, BUFSIZE,0);
-            if ( res == 0 )
+            while (true)
             {
-                // pokud v res nic neni
-                break;
+                int res = recv(csocket, buff, BUFSIZE,0);
+                if ( res <= 0 )
+                {
+
+                    break;
+                }
+
+                else if ( res == EAGAIN )
+                {
+                    cerr << "." << endl;
+                    continue;
+                }
+            }
+            cout << buff << endl;
+            if ( strlen(buff) == 0)
+            {
+                error_resv = 1; // dame klientovi vedet, ze je neco spatne
             }
 
             FILE *f;
             f = fopen( "/etc/passwd", "r" );
             if ( f == NULL )
             {
-                fprintf(stderr, "Problems with opening /etc/passwd/\n");
+                cerr << "Problems with opening /etc/passwd/" << endl;
                 exit(EXIT_FAILURE);
             }
 
             stringstream os;
             os.str( buff);
-
             MessageParse mess(os);
-            mess.proceedOutput();
-            string output = mess.getOutput();
-            cout << "OUTPUT: " << mess.getOutput() << endl;
-            cout << "Sleeping 10sec" << endl;
-            cout << output.length() << endl;
-            //sleep(10);
-            // cout << "OUTPUT_DATA: " << mess.getOutputData() << endl;
-            //cout << "OPTION: " << mess.getOption() << endl;
-            //cout << "LOGIN: " << mess.getLogin() << endl;
-
-            // vystupni out pro posilani dat klientovi
-            char out[ output.length() ];
-            strcpy(out, output.c_str() );
-            send(csocket, out, strlen(out), 0);
+            if ( error_resv )
+            {
+                mess.err_message = "Get no data from client, probably problem in send on client side or recv on server side, please, try again";
+                mess.error = 1;
+            } else {
+                mess.proceedOutput();
+            }
+            mess.sendData( csocket);
             fclose(f);
-
-
         }
         else
         {
             printf(".");
         }
-        printf("Connection to %s closed\n",str);
         close(csocket);
     }
 }
