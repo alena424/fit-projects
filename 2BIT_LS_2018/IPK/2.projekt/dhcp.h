5bc1df7678dd971350de84f9be9860fcc6c3a920
Alena Tesarova <xtesar36@stud.fit.vutbr.cz>
2018-04-03 22:18:52 +0200

    ipk2

--- 2BIT_LS_2018/IPK/2.projekt/dhcp.h
+++ 2BIT_LS_2018/IPK/2.projekt/dhcp.h
@@ -1,26 +1,26 @@
-/*
-* headers to work with DHCP server
-* @date
-* @author
+/**
+* dhcp.h headers to work with DHCP server
+* @date 03.04.2018
+* @author Alena Tesarova
 */
 
-#define VARIABLE_L 312 // length of variable in options in dhcp packet
-#define MAC_ADDRESS_L 6 // length of mac address
+#define VARIABLE_L      312 // length of variable in options in dhcp packet
+#define MAC_ADDRESS_L   6 // length of mac address
 
 /*  message type */
-#define BOOTREQUEST 1
-#define BOOTREAPLY 2
+#define BOOTREQUEST     1
+#define BOOTREAPLY      2
 
 /* Hardware address type */
-#define HTYPE_ETHER 1
+#define HTYPE_ETHER     1
 
 /* Hardware address length */
 // 6 for 10mb ethernet
-#define HLEN_ETHER 6
+#define HLEN_ETHER      6
 
 /* Broadcast flag */
 // first broadcast bit is set to one
-#define BROADCAST 0x8000
+#define BROADCAST       0x0800
 
 /* DHCP message type, page 24 RFC 1533
   Code   Len  Type
@@ -28,44 +28,54 @@
   |  53 |  1  | 1-7 |
   +-----+-----+-----+
 */
-#define DHCP_MESSAGE_TYPE 53
-#define DHCP_MESSAGE_LEN 1
-#define MSG_DISCOVER 	1
+#define DHCP_MESSAGE_TYPE   53
+#define DHCP_MESSAGE_LEN    1
+#define MSG_DISCOVER 	    1
 #define MSG_OFFER			2
-#define MSG_REQUEST 	3
-#define MSG_DECLINE 	4
+#define MSG_REQUEST 	    3
+#define MSG_DECLINE 	    4
 #define MSG_ACK 			5
 #define MSG_NACK			6
-#define MSG_RELEASE 	7
+#define MSG_RELEASE 	    7
 
-/* DHCP options codes, from page 2 in RFC 1533 */
-#define SUBNET_MASK 1 // length 4 bytes
-#define TIME_OFFSET 2 //length 4 bytes
+/* DHCP options codes, from page 2 in RFC 1533 might be needed */
+/*#define SUBNET_MASK 1 // length 4 bytes
+#define TIME_OFFSET 2 //length 4 bytes*/
 
+extern unsigned char ifmac[6];
 
 // see RFC 2131, page 8
-
-typedef struct dhcp_packet{
-	uint8_t op; // message type, 1 - BOOTREQUEST, 2 - BOOTREAPLY
-	uint8_t htype; // hardware address type
-	uint8_t hlen; // hardware address length
-	uint8_t htops; // client sets to zero, optionally used by relay agents
-	uint32_t xid; // transaction id, a random number chosen by the client, used by the client and server to associate message and responses beteer a client and a server
-	uint16_t sec; // filled in by a client, seconds since client began address getting or renewal
-	uint16_t flags;			// must be set to zero, leftmost bit is defined as broadcast
-	uint32_t ciaddr; // client IP address, only if client is in BOUND, RENEW, REBINDING state
-	uint32_t yiaddr; // "your" client IP address
-	uint32_t siaddr; // address of the server to use in the next step of the client's bootstrap process
-	uint32_t giaddr; //realy agend IP address
-	uint32_t chaddr; // client hardware address
-	uint32_t sname[64]; //optional server host name
-	uint32_t file[128]; // boot file name
-	uint32_t options[VARIABLE_L];
+typedef struct dhcp_packet
+{
+    uint8_t op;
+    uint8_t htype;
+    uint8_t hlen;
+    uint8_t hops;
+    uint32_t xid;
+    uint16_t secs;
+    uint16_t flags;
+    uint32_t ciaddr;
+    uint32_t yiaddr;
+    uint32_t siaddr;
+    uint32_t giaddr;
+    uint8_t chaddr[16];
+    uint8_t sname[64];
+    uint8_t file[128];
+    uint8_t options[VARIABLE_L];
 } dhcp_packet;
 
 //int dhcp_discover( dhcp_packet* dhcp, struct dhcp_lease *lease  );
 //int dhcp_request( dhcp_packet* dhcp, struct dhcp_lease *lease );
-int request_lease(int socket_send, int socket_recv, unsigned char * mac, unsigned char *dstmac, int max_tries, unsigned char *interface_mac, int iface_index);
+//int request_lease(int socket_send, int socket_recv, unsigned char * mac, unsigned char *dstmac, int max_tries, int iface_index);
+
+int request_lease(int sock_send,
+                  int sock_recv,
+                  const unsigned char* mac,
+                  //unsigned char* dstmac,
+                  //long timeout,
+                  int retries,
+                  int interface_index );
 
 
- /*** end of dhcp.h ***/
+
+/*** end of dhcp.h ***/
--- 2BIT_LS_2018/IPK/2.projekt/ipk-dhcpstarve.c
+++ 2BIT_LS_2018/IPK/2.projekt/ipk-dhcpstarve.c
@@ -1,4 +1,4 @@
-/*
+/**
  * IPK 2. project
  * @date 28.03.2018
  * @author Alena Tesarova
@@ -10,30 +10,31 @@
 #include "dhcp.h"
 //#include "leases.h"
 
-#define MAX_TRIES 10
+#define MAX_TRIES 2
 
-/*
-* function request_lease
-* @param socket_send socket we use to send DHCPDISCOVER packet
-* @param socket_recv socket to get DHCPOFFER packet
-* @param mac out mac address
-* @param dstmac mac address of interface
+unsigned char ifmac[6];
+/**
+* close_socket
+* function closes socket
+* @param socket
 */
-/*int request_lease( int socket_send, int socket_recv, unsigned char* mac, unsigned char* dstmac ){
-			// we first creates dhcp packet
+int close_socket(int sock)
+{
+    close(sock);
+    return EXIT_SUCCESS;
+}
 
-		}*/
-
-
-/*
+/**
+* generate_mac_addr
 * Function generates random mac address
 */
-unsigned char * generate_mac_addr(){
+unsigned char * generate_mac_addr()
+{
     unsigned char new_mac_addr[MAC_ADDRESS_L];
-    for( int i = 0; i < MAC_ADDRESS_L; i ++) {
+    for( int i = 0; i < MAC_ADDRESS_L; i ++)
+    {
         // number cant be above 15 in hexadecimal, 255 in descimal
         unsigned int rand_number = rand() % 255;
-        //:1printf("%d ", rand_number);
         new_mac_addr[i] = rand_number;
     }
     printf("New Random Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , new_mac_addr[0], new_mac_addr[1], new_mac_addr[2], new_mac_addr[3], new_mac_addr[4], new_mac_addr[5] );
@@ -41,148 +42,146 @@ unsigned char * generate_mac_addr(){
 
 }
 
-int main ( int argc, char *argv[] ){
-	/// inicialized random generator
+int main ( int argc, char *argv[] )
+{
+    /// inicialized random generator
 
-	srand( time(NULL) );
+    srand( time(NULL) );
 
- 	// enter: ./ipk-dhcpstarve -i interface
-	// interface: name of interface to generate attack
-	if ( argc != 3 ){
-		fprintf(stderr, "Bad number of arguments");
-		return(EXIT_FAILURE);
-	}
-	if ( strcmp( argv[ 1 ], "-i" ) != 0 ){
-		fprintf(stderr, "Bad number of arguments");
-		return(EXIT_FAILURE);
-	}
-	char interfacename[50];
-	strncpy( interfacename, argv[2], sizeof( interfacename ) );
-	printf( "Interface is: %s\n", interfacename );
+    // enter: ./ipk-dhcpstarve -i interface
+    // interface: name of interface to generate attack
+    if ( argc != 3 )
+    {
+        fprintf(stderr, "Bad number of arguments");
+        return(EXIT_FAILURE);
+    }
+    if ( strcmp( argv[ 1 ], "-i" ) != 0 )
+    {
+        fprintf(stderr, "Bad number of arguments");
+        return(EXIT_FAILURE);
+    }
+    char interfacename[50];
+    strncpy( interfacename, argv[2], sizeof( interfacename ) );
+    printf( "Interface is: %s\n", interfacename );
 
-	// crete sockets to send and receive data
-	int socket_recv = -1;
-	int socket_send = -1;
+    // crete sockets to send and receive data
+    int socket_recv = -1;
+    int socket_send = -1;
 
-	/* RECEIVE SOCKET */
+    /* RECEIVE SOCKET */
 
-	// soket on UDP
-	if (( socket_recv = socket( PF_PACKET, SOCK_DGRAM, htons( ETH_P_IP ) ) ) <= 0 ){
-		perror("Can not create recv socket\n");
-		//fprintf( stderr, "Can not create recv socket" );
-		return ( EXIT_FAILURE );
+    // soket on UDP
+    if (( socket_recv = socket( PF_PACKET, SOCK_DGRAM, htons( ETH_P_IP ) ) ) <= 0 )
+    {
+        perror("Can not create recv socket\n");
+        return ( EXIT_FAILURE );
 
-	}
+    }
 
-	// set options to receive socket
-	int sockopt = 1;
-	if ( setsockopt( socket_recv, SOL_SOCKET, SO_BROADCAST, &sockopt, sizeof( sockopt ) ) != 0 ){
-		perror("Can not set any options to receive socket\n");
-		//fprintf( stderr, "Can not set any options to receive socket" );
-		//close(socket_recv);
-		return (EXIT_FAILURE);
-	}
-	/* SENDING SOCKET */
+    // set options to receive socket
+    int sockopt = 1;
+    if ( setsockopt( socket_recv, SOL_SOCKET, SO_BROADCAST, &sockopt, sizeof( sockopt ) ) != 0 )
+    {
+        perror("Can not set any options to receive socket\n");
+        return (EXIT_FAILURE);
+    }
+    /* SENDING SOCKET */
     // raw netword protocol access
-	if ( ( socket_send  = socket( PF_PACKET, SOCK_RAW, htons( ETH_P_ALL ) ) ) == -1 ){
-fprintf( stderr, "Can not create send socket\n" );
-		return( EXIT_FAILURE );
-	}
-	// options for send socket
-	sockopt = 1;
-
-	if ( ( setsockopt( socket_send, SOL_SOCKET, SO_BROADCAST, &sockopt, sizeof( sockopt )  ) ) != 0 ){
-	 	perror(  "Can not not set any options to socket\n");
-		return(EXIT_FAILURE);
-	}
-
-
-	// get network interface index which name is in interfacename
-	// socket must contain packet socket descriptor
-	/*
-	   struct ifreq {
-	   char ifr_name[IFNAMSIZ]; innterface name
-	union {
-		struct sockaddr ifr_addr;
-		struct sockaddr ifr_dstaddr;
-		struct sockaddr ifr_broadaddr;
-		struct sockaddr ifr_netmask;
-		struct sockaddr ifr_hwaddr;
-		short           ifr_flags;
-		int             ifr_ifindex;
-		int             ifr_metric;
-		int             ifr_mtu;
-		struct ifmap    ifr_map;
-		char            ifr_slave[IFNAMSIZ];
-		char            ifr_newname[IFNAMSIZ];
-		char           *ifr_data;
-	};
-}
-	 */
-	struct ifreq ifr;
-	memset(&ifr, 0, sizeof(ifr));
-	strncpy( ifr.ifr_name, interfacename, sizeof( ifr.ifr_name ) );
-	if ( ioctl( socket_send, SIOCGIFINDEX, &ifr ) == -1 ) {
-		fprintf( stderr, "ioctl error\n" );
-		return( EXIT_FAILURE );
-	}
-	//index je v ifr.ifr_ifindex
-	printf("Index of interface: %d\n", ifr.ifr_ifindex);
-
-	// we need destination mac address
-	if ( ioctl( socket_send, SIOCGIFHWADDR, &ifr ) == -1 ){
-		fprintf(stderr, "ioctl error\n");
-		return( EXIT_FAILURE );
-	}
-	// mac adresa v ifr.ifr_hwaddr.sa_data (unsigned char *)
-	unsigned char * destmac;
-	destmac = malloc( MAC_ADDRESS_L);
-	if ( destmac == NULL ){
-		fprintf( stderr, "Error in malloc\n" );
-		return(EXIT_FAILURE);
-	}
-	destmac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
-	int interface_index = ifr.ifr_ifindex;
-	printf("Mac Interface: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , destmac[0], destmac[1], destmac[2], destmac[3], destmac[4], destmac[5] );
-	printf("Index is: %d\n", interface_index );
-	//strncpy( destmac, ifr.ifr_hwaddr.sa_data, MAC_ADDRESS_L );
-	//printf( "Destination mac address: %s\n", ifr.ifr_hwaddr.sa_data );
-	/*
-	struct dhcp_packet{
-		!!!
-	}
-	   */
-	int poc = 5;
-	//struct dhcp_lease* list = NULL;
-	;
-	while(poc){
-		poc--;
-
-		/* list of leases
-		need to send a DHCPDISCOVER packet with reqiest of IP addr
-		reply to out request i s a DHCOFFER with offer if IP addr, client can choose one IP addr
-		and request for the addr with DHCPREQUEST packet
-		server returns DHCPACK as a reply
-		*/
-		//renew all leases
-
-		// generate mac address
-		//strncpy( mac, generate_mac_addr(), MAC_ADDRESS_L);
-		unsigned char *mac = generate_mac_addr();
-		printf("Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );
-
-		// request lease
-		request_lease(socket_send, socket_recv, mac, destmac, MAX_TRIES, destmac, interface_index );
+    if ( ( socket_send  = socket( PF_PACKET, SOCK_RAW, htons( ETH_P_ALL ) ) ) == -1 )
+    {
+        perror ( "Can not create send socket\n" );
+        return( EXIT_FAILURE );
+    }
+    // options for send socket
+    sockopt = 1;
+    if ( ( setsockopt( socket_send, SOL_SOCKET, SO_BROADCAST, &sockopt, sizeof( sockopt )  ) ) != 0 )
+    {
+        perror(  "Can not not set any options to socket\n");
+        return(EXIT_FAILURE);
+    }
 
 
-		// we not successful, brak while
-	}
-	// end
+    // get network interface index which name is in interfacename
+    // socket must contain packet socket descriptor
+    /*
+       struct ifreq {
+       char ifr_name[IFNAMSIZ]; innterface name
+    union {
+    	struct sockaddr ifr_addr;
+    	struct sockaddr ifr_dstaddr;
+    	struct sockaddr ifr_broadaddr;
+    	struct sockaddr ifr_netmask;
+    	struct sockaddr ifr_hwaddr;
+    	short           ifr_flags;
+    	int             ifr_ifindex;
+    	int             ifr_metric;
+    	int             ifr_mtu;
+    	struct ifmap    ifr_map;
+    	char            ifr_slave[IFNAMSIZ];
+    	char            ifr_newname[IFNAMSIZ];
+    	char           *ifr_data;
+    };
+    }
+     */
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy( ifr.ifr_name, interfacename, sizeof( ifr.ifr_name ) );
+
+    // we need destination mac address
+    if ( ioctl( socket_send, SIOCGIFHWADDR, &ifr ) == -1 )
+    {
+        fprintf(stderr, "ioctl error\n");
+        return( EXIT_FAILURE );
+    }
+    memcpy( ifmac, ifr.ifr_hwaddr.sa_data, ETH_ALEN );
+    printf("interface: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , ifmac[0], ifmac[1], ifmac[2], ifmac[3], ifmac[4], ifmac[5] );
+
+    //memset(&ifr, 0, sizeof(ifr));
+    //strncpy( ifr.ifr_name, interfacename, sizeof( ifr.ifr_name ) );
+
+    if ( ioctl( socket_send, SIOCGIFINDEX, &ifr ) == -1 )
+    {
+        fprintf( stderr, "ioctl error\n" );
+        return( EXIT_FAILURE );
+    }
+    //index je v ifr.ifr_ifindex
+    printf("Index of interface: %d\n", ifr.ifr_ifindex);
 
 
+    // mac adresa v ifr.ifr_hwaddr.sa_data (unsigned char *)
+    unsigned char destmac[] = {0xff, 0xff, 0xff, 0xff,0xff, 0xff};
+    int interface_index = ifr.ifr_ifindex;
+    //printf("Mac desc: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , destmac[0], destmac[1], destmac[2], destmac[3], destmac[4], destmac[5] );
+    //printf("Index is: %d\n", interface_index );
+
+    int poc = 5;
+    while(poc)
+    {
+        poc--;
+
+        /* list of leases
+        need to send a DHCPDISCOVER packet with reqiest of IP addr
+        reply to out request i s a DHCOFFER with offer if IP addr, client can choose one IP addr
+        and request for the addr with DHCPREQUEST packet
+        server returns DHCPACK as a reply
+        */
+
+        // generate mac address
+
+        unsigned char *mac = generate_mac_addr();
+        //memcpy( ifmac, mac, ETH_ALEN );
+        printf("Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );
+
+        // request lease
+        request_lease(socket_send, socket_recv, generate_mac_addr(), 2, interface_index );
+        printf("request done\n");
 
 
+        // we not successful, brak while
+    }
+    // end
 
-	return 0;
+
+    return 0;
 
 }
