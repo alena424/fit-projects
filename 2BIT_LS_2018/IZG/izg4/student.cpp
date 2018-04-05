3188b4161d5cd56bcd2063e680256ca8dd835053
Tesarova Alena <xtesar36@PCM105-15.fit.vutbr.cz>
2018-04-05 13:38:00 +0200

    student.cpp

--- 2BIT_LS_2018/IZG/izg4/student.cpp
+++ 2BIT_LS_2018/IZG/izg4/student.cpp
@@ -2,7 +2,7 @@
  * Laborator 04 Krivky - Zaklady pocitacove grafiky - IZG
  * ihulik@fit.vutbr.cz
  *
- *
+ * 
  * Popis: Hlavicky funkci pro funkce studentu
  *
  * Opravy a modifikace:
@@ -52,27 +52,27 @@ void initControlPointsUp(S_Vector **points, int offset_x, int offset_y) {
 
   p.x = 40;  p.y = -250; point2d_vecPushBack(*points, p);
   p.x = 160; p.y = -250; point2d_vecPushBack(*points, p);
-
+  
   p.x = 200; p.y = 0;    point2d_vecPushBack(*points, p);
 
   p.x = 210; p.y = -180; point2d_vecPushBack(*points, p);
   p.x = 350; p.y = -180; point2d_vecPushBack(*points, p);
-
+  
   p.x = 360; p.y = 0;    point2d_vecPushBack(*points, p);
 
   p.x = 390; p.y = -120;  point2d_vecPushBack(*points, p);
   p.x = 430; p.y = -120;  point2d_vecPushBack(*points, p);
-
+  
   p.x = 460; p.y = 0;    point2d_vecPushBack(*points, p);
 
   p.x = 470; p.y = -70;  point2d_vecPushBack(*points, p);
   p.x = 525; p.y = -70;  point2d_vecPushBack(*points, p);
-
+  
   p.x = 535; p.y = 0;    point2d_vecPushBack(*points, p);
 
   p.x = 545; p.y = -40;  point2d_vecPushBack(*points, p);
   p.x = 575; p.y = -40;  point2d_vecPushBack(*points, p);
-
+  
   p.x = 585; p.y = 0;    point2d_vecPushBack(*points, p);
 
   Point2d offset = {offset_x, offset_y, 1.0};
@@ -88,40 +88,48 @@ void initControlPointsDown(S_Vector **points, int offset_x, int offset_y) {
    * ktery inicializuje ridici body tak, aby byla trajektorie spojita (C1). Muzete skopirovat
    * kod funkce initControlPointsUp(.) a upravit primo souradnice bodu v kodu.
    */
-   *points = vecCreateEmpty(sizeof(Point2d));
+  *points = vecCreateEmpty(sizeof(Point2d));
   Point2d p;
-  p.x = 0;   p.y = 0;    point2d_vecPushBack(*points, p);
+  p.x = 0;   p.y = 0;    point2d_vecPushBack(*points, p);//p0
 
-  p.x = 40;  p.y = -250; point2d_vecPushBack(*points, p);
-  p.x = 160; p.y = -250; point2d_vecPushBack(*points, p);
+  p.x = 40;  p.y = -250; point2d_vecPushBack(*points, p);//p1
+  p.x = 160; p.y = -250; point2d_vecPushBack(*points, p);//p2
+  
+  p.x = 200; p.y = 0;    point2d_vecPushBack(*points, p);//p3
 
-  p.x = 200; p.y = 0;    point2d_vecPushBack(*points, p);
+  p.x = 228.8; p.y = 180; point2d_vecPushBack(*points, p);//p4
+  // 180*40/250 = 28.8 (vzdalenost od P3)
+  // 28.8 + 200
+  p.x = 360-28.8; p.y = 180; point2d_vecPushBack(*points, p);//p5
+  
+  p.x = 360; p.y = 0;    point2d_vecPushBack(*points, p);//p6
+  //(331.2, 180)
+  // vektor (p6-p5)= (360-331.2, 0-180) = (28.8, -180) => (-28.8,180)
+  // (x, -120) => x = (-120*-28.8 / 180)
+  p.x = -120*-28.8 / 180 + 360; p.y = -120;  point2d_vecPushBack(*points, p);//p7
+  p.x = 460 - -120*-28.8 / 180 ; p.y = -120;  point2d_vecPushBack(*points, p);//p8
+  
+  p.x = 460; p.y = 0;    point2d_vecPushBack(*points, p);//p9
 
-  p.x = 210; p.y = -180; point2d_vecPushBack(*points, p);
-  p.x = 350; p.y = -180; point2d_vecPushBack(*points, p);
-
-  p.x = 360; p.y = 0;    point2d_vecPushBack(*points, p);
-
-  p.x = 390; p.y = -120;  point2d_vecPushBack(*points, p);
-  p.x = 430; p.y = -120;  point2d_vecPushBack(*points, p);
-
-  p.x = 460; p.y = 0;    point2d_vecPushBack(*points, p);
-
-  p.x = 470; p.y = -70;  point2d_vecPushBack(*points, p);
-  p.x = 525; p.y = -70;  point2d_vecPushBack(*points, p);
-
-  p.x = 535; p.y = 0;    point2d_vecPushBack(*points, p);
-
-  p.x = 545; p.y = -40;  point2d_vecPushBack(*points, p);
-  p.x = 575; p.y = -40;  point2d_vecPushBack(*points, p);
-
+  // (440.8, -120)
+  // vektor (p9-p8) = (460-440.8,0+120) = (20,120)
+  // (x, 70) => x = ( 70*20/120 ) = 11,6 ... +460	
+  p.x = 460 + 11.6; p.y = 70;  point2d_vecPushBack(*points, p);//p10
+  p.x = 535 - 11.6; p.y = 70;  point2d_vecPushBack(*points, p);//p11
+  
+  p.x = 535; p.y = 0;    point2d_vecPushBack(*points, p);//p12
+  // (523.4, 70)
+  // vektor (p12 - p11) = (11.6, -70) => (-11,6,70)
+  // (x, -40) => x = ( -40*-11.6/70 ) = 6.63 ... + 535
+  p.x = 535 + 6.63; p.y = -40;  point2d_vecPushBack(*points, p);
+  p.x = 585 - 6.63; p.y = -40;  point2d_vecPushBack(*points, p);
+  
   p.x = 585; p.y = 0;    point2d_vecPushBack(*points, p);
 
   Point2d offset = {offset_x, offset_y, 1.0};
   for(int i = 0; i < (*points)->size; i++) {
     addPoint2d(point2d_vecGetPtr(*points, i), &offset, point2d_vecGetPtr(*points, i));
   }
-  //initControlPointsUp(points, offset_x, offset_y);
 }
 
 /**
@@ -130,45 +138,47 @@ void initControlPointsDown(S_Vector **points, int offset_x, int offset_y) {
  * @param quality pocet bodu na krivke, ktere chceme vypocitat
  * @param trajectory_points vystupni vektor bodu kubiky (nemazat, jen pridavat body)
  */
-void bezierCubic(const Point2d *P0, const Point2d *P1, const Point2d *P2, const Point2d *P3,
+void bezierCubic(const Point2d *P0, const Point2d *P1, const Point2d *P2, const Point2d *P3, 
   const int quality, S_Vector *trajectory_points) {
-
+	
+  
   /* == TODO ==
    * Soucast Ulohy c.1:
    * Sem pridejte kod vypoctu Bezierove kubiky. Body krivky pridavejte do trajectory_points.
    */
-	for ( int t = 0; t < quality; t += 1 ){
-		Point2d p ; //= Point2d{0.0,0.0,1.0};
-		p.x = P0->x * pow(1-t, 3) + P1->x * 3 * t * pow(1-t, 2) + P2->x * 2 * t * (1-t) + P3->x * pow(t, 3);
-		p.y = P0->y * pow(1-t, 3) + P1->y * 3 * t * pow(1-t, 2) + P2->y * 2 * t * (1-t) + P3->y * pow(t, 3);
-		point2d_vecPushBack(trajectory_points, p );
+	for ( double t = 0.0; t < 1.0; t += 1.0/quality ){
+	Point2d p = { 0.0,0.0,1.0 };
+	p.x = P0->x * pow( 1-t, 3 ) + P1->x * 3 * t * pow( 1-t, 2 ) + P2->x * 3*t*t*(1-t)+P3->x*pow(t,3);
+	p.y = P0->y * pow( 1-t, 3 ) + P1->y * 3 * t * pow( 1-t, 2 ) + P2->y * 3*t*t*(1-t)+P3->y*pow(t,3);
+	point2d_vecPushBack(trajectory_points, p);
 	}
+
 }
 
-/*
+/* 
  * Implementace vypoctu trajektorie, ktera se sklada z Bezierovych kubik.
  * @param quality pocet bodu krivky, ktere mame urcit
- * @param control_points ridici body krivky
- * @param trajectory_points vystupni body zakrivene trajektorie
+ * @param control_points ridici body krivky 
+ * @param trajectory_points vystupni body zakrivene trajektorie 
  */
 void	bezierCubicsTrajectory(int quality, const S_Vector *control_points, S_Vector *trajectory_points) {
   // Toto musi byt na zacatku funkce, nemazat.
   point2d_vecClean(trajectory_points);
 
+
   /* == TODO ==
    * Uloha c.1
    * Ziskejte postupne 4 ridici body a pro kazdou ctverici vypocitejte body Bezierovy kubiky.
-   *
-   * Kostra:*/
-     for(int i = 0; i < ( trajectory_points->size -1 ); i += 3) {
-       Point2d *P0 = point2d_vecGetPtr( trajectory_points, i );
-       Point2d *P1 = point2d_vecGetPtr( trajectory_points, i+1);
-       Point2d *P2 = point2d_vecGetPtr( trajectory_points, i+2 );
-       Point2d *P3 = point2d_vecGetPtr( trajectory_points, i+3 );
+   * */
+
+   for( int i = 0;i < vecSize( control_points ) - 1; i += 3){
+      Point2d *P0 = point2d_vecGetPtr( control_points, i );
+       Point2d *P1 = point2d_vecGetPtr( control_points, i +1 );
+       Point2d *P2 = point2d_vecGetPtr( control_points, i +2);
+       Point2d *P3 = point2d_vecGetPtr( control_points, i +3);
        bezierCubic(P0, P1, P2, P3, quality, trajectory_points);
      }
-
-  /* Nasledujici volani funkce getLinePoints(.) zmazte - je to jen ilustrace hranate trajektorie.
+   /* Nasledujici volani funkce getLinePoints(.) zmazte - je to jen ilustrace hranate trajektorie.
    */
   //getLinePoints(control_points, trajectory_points);
 }
